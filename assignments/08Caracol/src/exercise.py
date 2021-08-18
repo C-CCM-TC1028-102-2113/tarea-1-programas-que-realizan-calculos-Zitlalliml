4d4a890cec0abd27fd7c637679361fc7908497c4
Zitlalliml <89087259+Zitlalliml@users.noreply.github.com>
2021-08-18 17:17:36 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    
+print ('Dame los minutos: ')
+m = int(input())
+    
+t = float( m * 34.2 / 5.7) #realiza la conversión de mm a cm
+    
+print ('Centímentros recorridos: ' + str(t))
+    
+    
     pass
 
 if __name__ == '__main__':
