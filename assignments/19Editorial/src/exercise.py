68cb1f5d04fe1cf28c950fe6e005df53486914bc
Zitlalliml <89087259+Zitlalliml@users.noreply.github.com>
2021-08-21 01:01:22 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,13 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    print('Dame el número de palabras:')
+    print('Dame el número de palabras: ')
     p= int(input())
     p1= (475 / p)
 
     if p1 < 1:
         t1 =(p // 475)
         t2= ((t1 + 1) * 60)* 0.90
-        print('El costo de la publicación es:' + str(round(t2,4 )))
+        print('El costo de la publicación es: ' + str(round(t2,4 )))
     else:
         t3 = 60*0.90
         print('El costo de la publicación es: ' + str(round(t3,4 )))
