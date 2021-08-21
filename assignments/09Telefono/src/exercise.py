04bdf79bb266515ac961e7b9adb749af0bb82f48
Zitlalliml <89087259+Zitlalliml@users.noreply.github.com>
2021-08-20 23:40:39 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
+    
     #Leer los datos
+    print('Dame el número de mensajes: ')
+    m = int(input())
+    print('Dame el número de megas:')
+    mg = float(input())
+    print('Dame el número de minutos: ')
+    min = int(input())
+
+    tm =float( round(m * 0.80,4))
+    tmg =float( round(mg * 0.80,4))
+    tmin =float( round(min * 0.80,4))
+    t= float(tm + tmg + tmin)
+    print ('El costo mensual es: ' + str(t))
+
     pass
 
 
